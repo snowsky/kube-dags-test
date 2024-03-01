1a3159dd5a5feb1bc399e47f0c5cfcdeae41a059
biakonza <146449798+biakonza@users.noreply.github.com>
2024-03-01 16:57:31 -0600

    Update example_storage_account.py (#20)

--- examples/example_storage_account.py
+++ examples/example_storage_account.py
@@ -27,7 +27,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
 
 with DAG(
-    dag_id="example_bash_operator",
+    dag_id="example_storage_account_and_s3_bucket",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
@@ -42,7 +42,7 @@ with DAG(
     # [START howto_operator_bash]
     run_this = BashOperator(
         task_id="run_after_loop",
-        bash_command="echo 'New test contents' >> /source/test_file",
+        bash_command="echo 'Hello from the container, run_this_last!' >> /source/test.txt",
     )
     # [END howto_operator_bash]
 
@@ -56,12 +56,24 @@ with DAG(
         task >> run_this
 
     # [START howto_operator_bash_template]
-    also_run_this = BashOperator(
-        task_id="also_run_this",
-        bash_command='echo "ti_key={{ task_instance_key_str }}"',
+    also_run_this_1 = BashOperator(
+        task_id="also_run_this_1",
+        bash_command="echo 'Hello from the container, also_run_this!' >> /source-s3-konzaandssigrouppipelines/test.txt",
     )
     # [END howto_operator_bash_template]
-    also_run_this >> run_this_last
+    also_run_this_1 >> run_this_last
+
+    also_run_this_2 = BashOperator(
+        task_id="also_run_this_2",
+        bash_command="echo 'Hello from the container, also_run_this!' >> /source-s3-konzaandssigroupqa/test.txt",
+    )
+    also_run_this_2 >> run_this_last
+
+    also_run_this_3 = BashOperator(
+        task_id="also_run_this_3",
+        bash_command="echo 'Hello from the container, also_run_this!' >> /source-s3-konzaandssigroup/test.txt",
+    )
+    also_run_this_3 >> run_this_last
 
 # [START howto_operator_bash_skip]
 this_will_skip = BashOperator(
