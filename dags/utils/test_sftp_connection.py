ac11f6d24905d6e38622f7ae529af8e881f1478a
biakonza <146449798+biakonza@users.noreply.github.com>
2024-04-30 16:58:25 -0500

    Update test_sftp_connection.py

--- dags/utils/test_sftp_connection.py
+++ dags/utils/test_sftp_connection.py
@@ -17,19 +17,21 @@ def test_connection_dag():
     @task
     def test_connection_task(ssh_conn_id: str):
         import logging
-        logging.info(print('Starting Connection Test')
+        logging.info(print('Starting Connection Test'))
         hook = SFTPHook(
             ssh_conn_id=ssh_conn_id,
         )
         conn_success = hook.test_connection()
         if not conn_success:
-            logging.info(print('Connection Unsuccessful')
+            logging.info(print('Connection Unsuccessful'))
             raise RuntimeError('Connection Unsuccessful')
         else:
-            logging.info(print('Connection Successful')
+            logging.info(print('Connection Successful'))
             return conn_success
 
-
+    test_connection_dag(
+        ssh_conn_id="{{params.ssh_conn_id}}",
+    )
 test_sftp_connection_dag = test_connection_dag()
 
 if __name__ == "__main__":
