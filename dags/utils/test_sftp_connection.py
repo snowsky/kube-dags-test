b980c92bd8a546d0eea617d82061cce78cb3690b
MialLewis <95620982+MialLewis@users.noreply.github.com>
2024-05-01 11:25:29 +1200

    Connect typo in connection dag (#36)
    * correct typo
    
    * get bool from return val

--- dags/utils/test_sftp_connection.py
+++ dags/utils/test_sftp_connection.py
@@ -2,6 +2,7 @@
 from datetime import timedelta, datetime
 from airflow.decorators import dag, task
 from airflow.providers.sftp.hooks.sftp import SFTPHook
+import logging
 
 
 @dag(
@@ -16,22 +17,22 @@ from airflow.providers.sftp.hooks.sftp import SFTPHook
 def test_connection_dag():
     @task
     def test_connection_task(ssh_conn_id: str):
-        import logging
-        logging.info(print('Starting Connection Test'))
+        logging.info('Starting Connection Test')
         hook = SFTPHook(
             ssh_conn_id=ssh_conn_id,
         )
         conn_success = hook.test_connection()
-        if not conn_success:
-            logging.info(print('Connection Unsuccessful'))
+        if not conn_success[0]:
+            logging.info('Connection Unsuccessful')
             raise RuntimeError('Connection Unsuccessful')
         else:
-            logging.info(print('Connection Successful'))
+            logging.info('Connection Successful')
             return conn_success
 
-    test_connection_dag(
+    test_connection_task(
         ssh_conn_id="{{params.ssh_conn_id}}",
     )
+
 test_sftp_connection_dag = test_connection_dag()
 
 if __name__ == "__main__":
