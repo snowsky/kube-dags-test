ece37d35ac127637c46a86f396be5043703797a2
biakonza <146449798+biakonza@users.noreply.github.com>
2024-06-03 15:42:05 -0500

    Update optout_load.py

--- dags/various/optout_load.py
+++ dags/various/optout_load.py
@@ -47,6 +47,11 @@ def optout_load():
         print(f"Moved file {file_path} to {destination_path}")
     @task
     def get_opt_out_list() -> pd.DataFrame:
+        hookOOL = MySqlHook(mysql_conn_id="prd-az1-sqlw2-airflowconnection")
+        hookOOL.run(sql="""START TRANSACTION; 
+            truncate clientresults.opt_out_list_airflow_load;
+            COMMIT;
+                """)
         logging.info(print('/source-biakonzasftp/'))
         sourceDir = '/source-biakonzasftp/C-9/optout_load/'
         optOutFiles = os.listdir(sourceDir)
@@ -96,6 +101,7 @@ def optout_load():
     def adjustLoad():
         hook = MySqlHook(mysql_conn_id="prd-az1-sqlw2-airflowconnection")
         hook.run(sql="""START TRANSACTION; 
+
             update clientresults.opt_out_list_airflow_load
                 set dob = date_format(str_to_date(dob, '%m/%d/%Y'),'%Y-%m-%d')
                 where dob <> 'Unknown' and dob not like '%-%'; 
@@ -103,12 +109,243 @@ def optout_load():
             UPDATE clientresults.opt_out_list_airflow_load
                     SET ssn = replace(ssn, '-', '')
                     ;
+            UPDATE clientresults.opt_out_list_airflow_load
+                SET ssn = LPAD(ssn, 9, '0')
+                ;
+            insert into clientresults.opt_out_list_airflow_load (
+                fname
+                ,lname
+                ,dob
+                ,sex
+                ,SSN
+                ,respective_mrn
+                ,respective_vault
+                ,opt_choice
+                ,`status`
+                ,last_update_php
+                ) (
+                select 
+                fname
+                ,lname
+                ,dob
+                ,sex
+                ,SSN
+                ,respective_mrn
+                ,respective_vault
+                ,opt_choice
+                ,`status`
+                ,last_update_php
+                from clientresults.opt_out_list
+                where opt_choice <> 'in'
+                );
+            drop table if exists temp.opt_out_mpi_id_master_lookup
+            ;
+            create table temp.opt_out_mpi_id_master_lookup like person_master._mpi_id_master;
+
+            insert into temp.opt_out_mpi_id_master_lookup
+                select * from person_master._mpi_id_master;
+
+            drop table if exists clientresults.opt_out_list
+                ;
+            create table clientresults.opt_out_list
+                select 
+                Full_OOLU.id
+                ,MPI
+                ,Full_OOLU.fname
+                ,Full_OOLU.lname
+                ,Full_OOLU.dob
+                ,Full_OOLU.sex
+                ,Full_OOLU.SSN
+                ,Full_OOLU.respective_vault
+                ,Full_OOLU.respective_mrn
+                ,Full_OOLU.opt_choice
+                ,Full_OOLU.status
+                ,Full_OOLU.username
+                ,Full_OOLU.user
+                ,Full_OOLU.submitted
+                ,Full_OOLU.completed
+                ,Full_OOLU.last_update_php
+                from clientresults.opt_out_list_airflow_load Full_OOLU
+                ;
+            drop table if exists temp.optouttest_all_related_mpi_by_std
+            ;
+
+            create table temp.optouttest_all_related_mpi_by_std
+                SELECT
+                Full_OOLU.id
+                ,Full_OOLU.MPI
+                ,Full_OOLU.fname
+                ,Full_OOLU.lname
+                ,Full_OOLU.dob
+                ,Full_OOLU.sex
+                ,Full_OOLU.SSN
+                ,Full_OOLU.respective_vault
+                ,Full_OOLU.respective_mrn
+                ,Full_OOLU.opt_choice
+                ,Full_OOLU.status
+                ,Full_OOLU.username
+                ,Full_OOLU.user
+                ,Full_OOLU.submitted
+                ,Full_OOLU.completed
+                ,Full_OOLU.last_update_php
+                ,MPID.id MPID_ID
+                ,MPID.firstname
+                ,MPID.lastname
+                ,MPID.dob MPID_DOB
+                ,MPID.sex MPID_SEX
+                ,MPID.ssn MPID_SSN
+                ,MPID.longitudinal_anomoly
+                ,MPID.first_name_anomoly
+                ,MPID.mrn_anomoly
+                 FROM clientresults.opt_out_list_airflow_load Full_OOLU
+                left join person_master._mpi_id_master MPID on (
+                Full_OOLU.fname = MPID.firstname
+                and Full_OOLU.lname = MPID.lastname
+                and Full_OOLU.dob = MPID.dob
+                and Full_OOLU.sex = MPID.sex)
+                where Full_OOLU.dob is not null and Full_OOLU.lname is not null
+                ;
+            drop table if exists temp.optouttest_all_related_mpi_by_special1
+            ;
+
+            create table temp.optouttest_all_related_mpi_by_special1
+                SELECT
+                Full_OOLU.id
+                ,Full_OOLU.MPI
+                ,Full_OOLU.fname
+                ,Full_OOLU.lname
+                ,Full_OOLU.dob
+                ,Full_OOLU.sex
+                ,Full_OOLU.SSN
+                ,Full_OOLU.respective_vault
+                ,Full_OOLU.respective_mrn
+                ,Full_OOLU.opt_choice
+                ,Full_OOLU.status
+                ,Full_OOLU.username
+                ,Full_OOLU.user
+                ,Full_OOLU.submitted
+                ,Full_OOLU.completed
+                ,Full_OOLU.last_update_php
+                ,MPID.id MPID_ID
+                ,MPID.firstname
+                ,MPID.lastname
+                ,MPID.dob MPID_DOB
+                ,MPID.sex MPID_SEX
+                ,MPID.ssn MPID_SSN
+                ,MPID.longitudinal_anomoly
+                ,MPID.first_name_anomoly
+                ,MPID.mrn_anomoly
+                FROM clientresults.opt_out_list_airflow_load Full_OOLU
+                left join person_master._mpi_id_master MPID on (
+                Full_OOLU.fname = MPID.firstname
+                and left(Full_OOLU.lname,3) = left(MPID.lastname,3)
+                and Full_OOLU.dob = MPID.dob
+                and Full_OOLU.sex = MPID.sex
+                and right(Full_OOLU.ssn,4) = right(MPID.ssn,4))
+                where Full_OOLU.dob is not null and Full_OOLU.lname is not null
+                ;
+            drop table if exists clientresults.opt_out_list
+                ;
+
+            create table clientresults.opt_out_list
+                select
+                Full_OOLU.id
+                ,MPID_ID MPI
+                ,Full_OOLU.fname
+                ,Full_OOLU.lname
+                ,Full_OOLU.dob
+                ,Full_OOLU.sex
+                ,Full_OOLU.SSN
+                ,Full_OOLU.respective_vault
+                ,Full_OOLU.respective_mrn
+                ,Full_OOLU.opt_choice
+                ,Full_OOLU.status
+                ,Full_OOLU.username
+                ,Full_OOLU.user
+                ,Full_OOLU.submitted
+                ,Full_OOLU.completed
+                ,Full_OOLU.last_update_php
+                from temp.optouttest_all_related_mpi_by_std Full_OOLU
+                #union
+                #select
+                #Full_OOLU.id
+                #,MPID_ID MPI
+                #,Full_OOLU.fname
+                #,Full_OOLU.lname
+                #,Full_OOLU.dob
+                #,Full_OOLU.sex
+                #,Full_OOLU.SSN
+                #,Full_OOLU.respective_vault
+                #,Full_OOLU.respective_mrn
+                #,Full_OOLU.opt_choice
+                #,Full_OOLU.status
+                #,Full_OOLU.username
+                #,Full_OOLU.user
+                #,Full_OOLU.submitted
+                #,Full_OOLU.completed
+                #,Full_OOLU.last_update_php
+                #from temp.optouttest_all_related_mpi_by_ssn Full_OOLU
+                union
+                select
+                Full_OOLU.id
+                ,MPID_ID MPI
+                ,Full_OOLU.fname
+                ,Full_OOLU.lname
+                ,Full_OOLU.dob
+                ,Full_OOLU.sex
+                ,Full_OOLU.SSN
+                ,Full_OOLU.respective_vault
+                ,Full_OOLU.respective_mrn
+                ,Full_OOLU.opt_choice
+                ,Full_OOLU.status
+                ,Full_OOLU.username
+                ,Full_OOLU.user
+                ,Full_OOLU.submitted
+                ,Full_OOLU.completed
+                ,Full_OOLU.last_update_php
+                from temp.optouttest_all_related_mpi_by_special1 Full_OOLU
+                ;
+            drop table if exists temp.optoutshrink_airflow
+                ;
+            create table temp.optoutshrink_airflow
+                SELECT * FROM clientresults.opt_out_list
+                group by 
+                MPI
+                ,fname
+                ,lname
+                ,dob
+                ,sex
+                ,SSN
+                ,respective_vault
+                ,respective_mrn
+                ,opt_choice
+                ,status
+                ,username
+                ,user
+                ,submitted
+                ,completed
+                ,last_update_php
+                order by MPI, respective_mrn, last_update_php desc
+                ;
+            truncate clientresults.opt_out_list;
+            insert into clientresults.opt_out_list
+                select * from temp.optoutshrink_airflow
+                group by MPI
+                ,fname
+                ,lname
+                ,dob
+                ,sex
+                ,SSN
+                ,respective_vault
+                ,respective_mrn
+                ;
             COMMIT;
                 """)
        
     dfOptOuts = get_opt_out_list()
     print(dfOptOuts)
-    adjustLoad()
+    adjustedLoad = adjustLoad()
+    adjustedLoad << dfOptOuts ## establishes sequence of dfOptOuts first then adjustedLoad
     #@task
     #def get_local_dirs() -> pd.DataFrame:
     #    
