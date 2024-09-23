68d77eb34ccf49b717526f59f3d5307add609fda
MialLewis <95620982+MialLewis@users.noreply.github.com>
2024-09-24 10:31:32 +1200

    L69 dag - reduce number of mapped tasks (#83)
    * add launch script
    
    * add updated L69 dag with diff file fn
    
    * add s3 upload task
    
    * add copy task
    
    * rename dag
    
    * reduce number of mapped tasks
    
    * Changes made to : Loaction to S3 bucket file trasfer and DomainOID
    
    * add input dir sanitation
    
    * enable pool processing in task
    
    * split tasks into even batches
    
    * add task to identify successful transfers
    
    * archive files in batches
    
    * revert change to original l69 dag
    
    * add ability to skip awks buckets
    
    * change trigger rule
    
    * enable task to work when bucket is skipped
    
    * handle exception of duplicate filename, pool size adjusted
    
    ---------
    
    Co-authored-by: Aishwarya Agarwal <aagarwal@konza.org>

--- dags/C-126/C_126_L_69_mapped.py
+++ dags/C-126/C_126_L_69_mapped.py
@@ -1,12 +1,37 @@
 from airflow import DAG
 from airflow.decorators import task
 from airflow.models.param import Param
-from airflow.operators.python import get_current_context
+from airflow.utils.trigger_rule import TriggerRule
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from os import path, makedirs
+from functools import partial
+import math
+# If we want to utilise ProcessPoolExecutor we need to set
+# AIRFLOW__CORE__EXECUTE_TASKS_NEW_PYTHON_INTERPRETER = true
+from concurrent.futures import as_completed, ThreadPoolExecutor as PoolExecutor
 
-SOURCE_FILES_DIRECTORY='/data/biakonzasftp/C-126/L-69/source/'
-DEST_FILES_DIRECTORY='/data/biakonzasftp/C-126/L-69/dest/'
+DEFAULT_SOURCE_FILES_DIRECTORY = '/data/biakonzasftp/C-126/L-69/source/'
+DEFAULT_DEST_FILES_DIRECTORY = '/data/biakonzasftp/C-126/L-69/dest/'
+DEFAULT_MAX_POOL_WORKERS = 10
+DEFAULT_MAX_TASKS = 10
+PARALLEL_TASK_LIMIT = 5
+
+class BucketDetails:
+    def __init__(self, aws_conn_id, aws_key_pattern, s3_hook_kwargs):
+        self.aws_conn_id = aws_conn_id
+        self.aws_key_pattern = aws_key_pattern
+        self.s3_hook_kwargs = s3_hook_kwargs
+
+
+AWS_BUCKETS = {'konzaandssigrouppipelines':
+                   BucketDetails(aws_conn_id='konzaandssigrouppipelines',
+                                 aws_key_pattern='HL7v3Out/HL7InV3_CDA_KONZA_SFTP_Retrieval__L_69/{input_file}',
+                                 s3_hook_kwargs={}),
+               'com-ssigroup-insight-attribution-data':
+                   BucketDetails(aws_conn_id='konzaandssigrouppipelines',
+                                 aws_key_pattern='clientName=KONZA/source=L-69/status=pending/domainOid=2.16.840.1.113883.17.9491/{input_file_replaced}',
+                                 s3_hook_kwargs={'encrypt': True, 'acl_policy':'bucket-owner-full-control'})}
+
 
 default_args = {
     'owner': 'airflow',
@@ -16,41 +41,90 @@ with DAG(
     default_args=default_args,
     schedule=None,
     tags=['example', 'C-126', 'L-69'],
+    concurrency=PARALLEL_TASK_LIMIT,
     params={
-        "source_files_dir_path": Param(SOURCE_FILES_DIRECTORY, type="string"),
-        "output_files_dir_path": Param(DEST_FILES_DIRECTORY, type="string")
+        "source_files_dir_path": Param(DEFAULT_SOURCE_FILES_DIRECTORY, type="string"),
+        "output_files_dir_path": Param(DEFAULT_DEST_FILES_DIRECTORY, type="string"),
+        "max_pool_workers": Param(DEFAULT_MAX_POOL_WORKERS, type="integer", minimum=0),
+        "max_mapped_tasks": Param(DEFAULT_MAX_TASKS, type="integer", minimum=0),
+        "transer_to_konzaandssigrouppipelines_bucket": Param(True, type="boolean")
     },
 ) as dag:
     @task
-    def diff_files_task(params:dict):
+    def diff_files_task(params: dict):
+        _sanitise_input_directories(params)
         source_files = _get_files_from_dir(params['source_files_dir_path'])
         dest_files = _get_files_from_dir(params['output_files_dir_path'])
-        diff_files = [f for f in source_files if f not in dest_files]
-        return diff_files
+        unique_files = [f for f in source_files if f not in dest_files]
+        if unique_files:
+            return _split_list_into_batches(unique_files, params['max_mapped_tasks'])
+        else:
+            return []
+
+    def _split_list_into_batches(target_list,  max_tasks):
+        chunk_size = math.ceil(len(target_list) / max_tasks)
+        batches = [target_list[i:i + chunk_size] for i in range(0, len(target_list), chunk_size)]
+        return batches
+
+    def _sanitise_input_directories(params):
+        params['source_files_dir_path'] = _sanitise_dir(params['source_files_dir_path'])
+        params['output_files_dir_path'] = _sanitise_dir(params['output_files_dir_path'])
+
+    def _sanitise_dir(dir):
+        prefix = "" if dir[0] == "/" else "/"
+        suffix = "" if dir[-1] == "/" else "/"
+        return f'{prefix}{dir}{suffix}'
 
     def _get_files_from_dir(target_dir):
         import glob
         file_paths = [f.replace(target_dir, '') for f in glob.iglob(f'{target_dir}/**/*', recursive=True) if path.isfile(f)]
         return file_paths
 
-    @task(map_index_template="{{ input_file }}")
-    def copy_file_task(input_file, params:dict):
+    @task(trigger_rule=TriggerRule.NONE_FAILED)
+    def copy_file_task(input_file_list, params: dict):
+        max_workers = params['max_pool_workers']
+        with PoolExecutor(max_workers=max_workers) as executor:
+            future_file_dict = {executor.submit(partial(_copy_file, params), f): f for f in input_file_list}
+            return _get_results_from_futures(future_file_dict)
+
+    def _copy_file(params, file):
         import shutil
-        context = get_current_context()
-        context["input_file"] = input_file
-        input_file_path = path.join(params['source_files_dir_path'], input_file)
-        dest_file_path = path.join(params['output_files_dir_path'], input_file)
+        input_file_path = path.join(params['source_files_dir_path'], file)
+        dest_file_path = path.join(params['output_files_dir_path'], file)
         makedirs(path.dirname(dest_file_path), exist_ok=True)
         shutil.copy2(input_file_path, dest_file_path)
+        return file
 
-    @task(map_index_template="{{ input_file }}")
-    def upload_file_to_s3_task(input_file, aws_conn_id, aws_key_pattern, aws_bucket_name, s3_hook_kwargs, params:dict):
-        context = get_current_context()
-        context["input_file"] = input_file
-        input_file_path = path.join(params['source_files_dir_path'], input_file)
+    def _get_results_from_futures(future_file_dict):
+        results = []
+        exceptions = []
+        for future in as_completed(future_file_dict):
+            file = future_file_dict[future]
+            try:
+                results.append(future.result())
+            except Exception as e:
+                exceptions.append(str(f'{file}: {str(e)})'))
+        if exceptions:
+            raise Exception(f'exceptions raised: {exceptions}')
+        return results
+
+    def create_upload_file_to_s3_task(bucket_name):
+        @task(task_id=bucket_name)
+        def upload_file_to_s3_task_def(input_file_list, aws_conn_id, aws_key_pattern, aws_bucket_name, s3_hook_kwargs,
+                                   params: dict):
+            max_workers = params['max_pool_workers']
+            with PoolExecutor(max_workers=max_workers) as executor:
+                future_file_dict = {executor.submit(partial(_upload_file_to_s3, params, aws_key_pattern, aws_conn_id,
+                                                            aws_bucket_name, s3_hook_kwargs), f): f for f in
+                                    input_file_list}
+                return _get_results_from_futures(future_file_dict)
+        return upload_file_to_s3_task_def
+
+    def _upload_file_to_s3(params, aws_key_pattern, aws_conn_id, aws_bucket_name, s3_hook_kwargs, file):
+        input_file_path = path.join(params['source_files_dir_path'], file)
         aws_key = aws_key_pattern
         # Add/edit replacements depending upon aws key pattern.
-        replacements = {"{input_file}": input_file, "{input_file_stub}": input_file.split('/', 1)[-1]}
+        replacements = {"{input_file}": file, "{input_file_replaced}": file.replace('/','__')}
         for r in replacements:
             aws_key = aws_key.replace(r, replacements[r])
         s3_hook = S3Hook(aws_conn_id=aws_conn_id)
@@ -60,19 +134,36 @@ with DAG(
             bucket_name=aws_bucket_name,
             **s3_hook_kwargs
         )
+        return file
+
+    @task(trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS)
+    def identify_successful_transfers_task(mapped_task_results, params: dict):
+        # A successful transfer is a file that was successfully transferred to ALL target s3 accounts.
+        # mapped task results expected a 3 tier list: 1) All tasks 2) Individual Task 3) Mapped task instances.
+        None in mapped_task_results and mapped_task_results.remove(None)  # Remove skipped tasks.
+        flat_list = [c for a in mapped_task_results for b in a for c in b]
+        successful_transfers = {r for r in flat_list if flat_list.count(r) == len(mapped_task_results)}
+        return _split_list_into_batches(list(successful_transfers), params['max_mapped_tasks'])
+
+    @task.branch
+    def select_buckets_task(params: dict):
+        buckets = AWS_BUCKETS
+        if not params['transer_to_konzaandssigrouppipelines_bucket']:
+            buckets.pop('konzaandssigrouppipelines')
+        return list(buckets.keys())
 
     diff_files = diff_files_task()
-    copy_file = copy_file_task.expand(input_file=diff_files)
-    transfer_file_to_s3 = upload_file_to_s3_task.expand(input_file=diff_files,
-                                                        aws_conn_id=['konzaandssigrouppipelines'],
-                                                        aws_key_pattern=['HL7v3Out/HL7InV3_CDA_KONZA_SFTP_Retrieval__L_69/Office Practicum/{input_file}'],
-                                                        aws_bucket_name=['konzaandssigrouppipelines'],
-                                                        s3_hook_kwargs = [{}])
-    transfer_file_to_s3_nashville = upload_file_to_s3_task.expand(input_file=diff_files,
-                                                                  aws_conn_id=['konzaandssigrouppipelines'],
-                                                                  aws_key_pattern=['clientName=KONZA/source=L-69/status=pending/domainOid={input_file_stub}'],
-                                                                  aws_bucket_name=['com-ssigroup-insight-attribution-data'],
-                                                                  s3_hook_kwargs=[{'encrypt': True, 'acl_policy':'bucket-owner-full-control'}])
+    select_buckets = select_buckets_task()
+    transfer_file_to_s3_tasks = []
+    for bucket in AWS_BUCKETS:
+        upload_file_to_s3_task = create_upload_file_to_s3_task(bucket)
+        transfer_file_to_s3 = upload_file_to_s3_task.expand(input_file_list=diff_files,
+                                                            aws_conn_id=[AWS_BUCKETS[bucket].aws_conn_id],
+                                                            aws_key_pattern=[AWS_BUCKETS[bucket].aws_key_pattern],
+                                                            aws_bucket_name=[bucket],
+                                                            s3_hook_kwargs=[AWS_BUCKETS[bucket].s3_hook_kwargs])
+        transfer_file_to_s3_tasks.append(transfer_file_to_s3)
+    identify_successful_transfers = identify_successful_transfers_task(mapped_task_results=transfer_file_to_s3_tasks)
+    archive_transferred_file = copy_file_task.expand(input_file_list=identify_successful_transfers)
 
-    # This establishes order of the stages. Comment to run all in parallel.
-    diff_files >> copy_file >> (transfer_file_to_s3, transfer_file_to_s3_nashville)
+    diff_files >> select_buckets >> transfer_file_to_s3_tasks >> identify_successful_transfers >> archive_transferred_file
