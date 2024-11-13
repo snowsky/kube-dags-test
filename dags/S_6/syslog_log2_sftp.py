fa078d2b78a18baa2ce7766fa2ed2b876a99282d
biakonza <146449798+biakonza@users.noreply.github.com>
2024-11-13 11:25:55 -0600

    Update syslog_log2_sftp.py

--- dags/S_6/syslog_log2_sftp.py
+++ dags/S_6/syslog_log2_sftp.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import os
 import shutil
 import pwd
+import paramiko
 
 # Define the DAG
 default_args = {
@@ -43,9 +44,14 @@ def copy_to_network_path(sftp_conn_id, sftp_path, network_path):
     if sftp_hook.isfile(os.path.join(sftp_path, file_name)):
         local_file_path = os.path.join(network_path_with_date, f'{file_name}_{day_stamp}')
         
-        # Change ownership to the current user
+        # Change ownership to the current user using paramiko
         current_user = pwd.getpwuid(os.getuid()).pw_name
-        sftp_hook.execute_command(f'chown {current_user}:{current_user} {os.path.join(sftp_path, file_name)}')
+        transport = paramiko.Transport((sftp_hook.host, sftp_hook.port))
+        transport.connect(username=sftp_hook.username, password=sftp_hook.password)
+        sftp = paramiko.SFTPClient.from_transport(transport)
+        sftp.chown(os.path.join(sftp_path, file_name), os.getuid(), os.getgid())
+        sftp.close()
+        transport.close()
         
         sftp_hook.retrieve_file(os.path.join(sftp_path, file_name), local_file_path)
         print(f'Copied {file_name} to {network_path_with_date} with day stamp {day_stamp}')
