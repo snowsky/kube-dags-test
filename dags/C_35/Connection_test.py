ede16a591aa6f153ffab1e6d2bf40051808e27a8
Blake Cook <162179568+bcook-konza@users.noreply.github.com>
2024-12-27 14:03:26 -0600

    undo hardcode dependencies test (#127)

--- dags/C_35/Connection_test.py
+++ dags/C_35/Connection_test.py
@@ -3,22 +3,11 @@ from airflow.providers.trino.operators.trino import TrinoOperator
 from datetime import datetime
 import subprocess
 
-# hardcode dependencies
-def install_dependencies():
-    subprocess.check_call(["pip", "install", "apache-airflow"])
-    subprocess.check_call(["pip", "install", "apache-airflow-providers-common-sql"])
-    subprocess.check_call(["pip", "install", "pandas"])
-    subprocess.check_call(["pip", "install", "trino"])
-    subprocess.check_call(["pip", "install", "pymssql"])
-
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2024, 12, 19),
     'retries': 1,
 }
-
-# install dependencies
-install_dependencies()
 
 with DAG('Connection_test_trino_dag', default_args=default_args, schedule='@daily', tags=['C-35']) as dag:
     trino_task = TrinoOperator(
