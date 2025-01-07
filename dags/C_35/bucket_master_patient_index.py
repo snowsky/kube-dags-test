a0b49a0078d5a55b6caac9a7fcbfccfccdc6f992
biakonza <146449798+biakonza@users.noreply.github.com>
2025-01-07 06:43:00 -0600

    Update bucket_master_patient_index.py

--- dags/C_35/bucket_master_patient_index.py
+++ dags/C_35/bucket_master_patient_index.py
@@ -51,6 +51,7 @@ def execute_trino_queries(**kwargs):
 with DAG(
     dag_id='bucket_master_patient_index',
     schedule_interval='@monthly',
+    max_active_runs=1,
     tags=['C-35'],
     start_date=datetime(2023, 3, 1),
     catchup=True,
