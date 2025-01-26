b790e553e1cfce6b55116c0b71edd38cade83fb3
biakonza <146449798+biakonza@users.noreply.github.com>
2025-01-26 11:43:50 -0600

    Update EDW_State_Report.py

--- dags/C_111/EDW_State_Report.py
+++ dags/C_111/EDW_State_Report.py
@@ -378,15 +378,19 @@ state varchar)
         accid_ref varchar, 
         index_update varchar
         WITH ( 
-        partition_projection_format = 'yyyy-MM', 
-        partition_projection_interval = 1, 
-        partition_projection_interval_unit = 'DAYS', 
-        partition_projection_range = ARRAY['2023-03','2024-10'], --'NOW' --can be used if in the current month EG. if there is no month folder for 2024-08, and it is August, must set a range to the prior month
-        partition_projection_type = 'DATE' ) ) WITH 
-        ( external_location = 'abfs://content@reportwriterstorage.dfs.core.windows.net/parquet-master-data/mpi',
-        format = 'PARQUET', partition_projection_enabled = true, 
-        partition_projection_location_template = 'abfs://content@reportwriterstorage.dfs.core.windows.net/parquet-master-data/mpi/${index_update}', 
-        partitioned_by = ARRAY['index_update'] )
+        partitioned_by = ARRAY['index_update'], 
+            bucketed_by = ARRAY['accid_ref'], 
+            sorted_by = ARRAY['accid_ref'],
+            bucket_count = 64 )
+        #partition_projection_format = 'yyyy-MM', 
+        #partition_projection_interval = 1, 
+        #partition_projection_interval_unit = 'DAYS', 
+        #partition_projection_range = ARRAY['2023-03','2024-10'], --'NOW' --can be used if in the current month EG. if there is no month folder for 2024-08, and it is August, must set a range to the prior month
+        #partition_projection_type = 'DATE' ) ) WITH 
+        #( external_location = 'abfs://content@reportwriterstorage.dfs.core.windows.net/parquet-master-data/mpi',
+        #format = 'PARQUET', partition_projection_enabled = true, 
+        #partition_projection_location_template = 'abfs://content@reportwriterstorage.dfs.core.windows.net/parquet-master-data/mpi/${index_update}', 
+        #partitioned_by = ARRAY['index_update'] )
         """,
     )
     drop_mpi_accid_no_blanks = KonzaTrinoOperator(
