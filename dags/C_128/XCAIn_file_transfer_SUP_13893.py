14b284aa0e1a7bd0a69f8135d806a9c325adc0b3
biakonza <146449798+biakonza@users.noreply.github.com>
2025-02-07 15:43:28 -0600

    Update XCAIn_file_transfer_SUP_13893.py

--- dags/C_128/XCAIn_file_transfer_SUP_13893.py
+++ dags/C_128/XCAIn_file_transfer_SUP_13893.py
@@ -444,7 +444,12 @@ def divide_files_into_batches(xml_files: List[str], batch_size: str) -> List[Lis
 files = list_files_in_s3()
 xml_files = filter_xml_files(files)
 batches = divide_files_into_batches(xml_files, batch_size="{{ params.batch_size }}")
-transfer_tasks = transfer_batch_to_sftp.expand(batch=batches)
+#transfer_tasks = transfer_batch_to_sftp.expand(batch=batches)
+transfer_tasks = CustomPythonOperator.partial(
+        task_id='transfer_batch_to_sftp',
+        python_callable=transfer_batch_to_sftp,
+        execution_timeout=timedelta(minutes=60)  # Set timeout for each mapped task to 60 minutes
+    ).expand(batch=batches)
 #download_files = download_files_to_local(xml_files, local_dir=LOCAL_DIR, aws_conn_id="konzaandssigrouppipelines", bucket_name=BUCKET_NAME, max_workers="{{ params.max_workers }}")
 delete_empty_directories_from_s3 = delete_empty_directories_from_s3(xml_files, aws_conn_id="konzaandssigrouppipelines", bucket_name=BUCKET_NAME)
 delete_empty_directories_from_s3.trigger_rule = TriggerRule.ONE_SUCCESS
