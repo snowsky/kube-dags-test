36b6f0305c78b6c9376a254563350642d30fae43
biakonza <146449798+biakonza@users.noreply.github.com>
2025-02-07 15:17:03 -0600

    Update XCAIn_file_transfer_SUP_13893.py

--- dags/C_128/XCAIn_file_transfer_SUP_13893.py
+++ dags/C_128/XCAIn_file_transfer_SUP_13893.py
@@ -434,14 +434,11 @@ def divide_files_into_batches(xml_files: List[str], batch_size: str) -> List[Lis
 
 
 
-
-
-
 # Define the workflow
 files = list_files_in_s3()
 xml_files = filter_xml_files(files)
 batches = divide_files_into_batches(xml_files, batch_size="{{ params.batch_size }}")
-transfer_tasks = transfer_batch_to_sftp.expand(batch=batches).set(
+transfer_tasks = transfer_batch_to_sftp.expand_kwargs(batch=batches).set(
         execution_timeout=timedelta(minutes=60))
 #download_files = download_files_to_local(xml_files, local_dir=LOCAL_DIR, aws_conn_id="konzaandssigrouppipelines", bucket_name=BUCKET_NAME, max_workers="{{ params.max_workers }}")
 delete_empty_directories_from_s3 = delete_empty_directories_from_s3(xml_files, aws_conn_id="konzaandssigrouppipelines", bucket_name=BUCKET_NAME)
