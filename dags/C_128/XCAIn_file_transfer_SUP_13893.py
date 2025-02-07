f34dce101608dbcfbae9e328456ba45e058235b8
biakonza <146449798+biakonza@users.noreply.github.com>
2025-02-07 15:46:46 -0600

    Update XCAIn_file_transfer_SUP_13893.py

--- dags/C_128/XCAIn_file_transfer_SUP_13893.py
+++ dags/C_128/XCAIn_file_transfer_SUP_13893.py
@@ -30,7 +30,7 @@ dag = DAG(
     catchup=False,
     params={
         "max_workers": Param(5, type="integer", minimum=1),
-        "batch_size  - Warning do not set higher than 500 (testing on 2/7 around duration greater than 1 hour hitting timeout defined below)": Param(100, type="integer", minimum=1)
+        "batch_size": Param(250, type="integer", minimum=1) #  - Warning do not set higher than 500 (testing on 2/7 around duration greater than 1 hour hitting timeout defined below)
     }
 )
 ENV = 'Prod'
