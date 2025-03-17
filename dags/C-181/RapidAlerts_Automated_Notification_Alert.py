7027cf83823b3e9a607ce2cb86417cf2e3387b31
Eric Thompson <ethompson@konza.org>
2025-03-17 21:52:40 +0000

    testing table cycle print

--- dags/C-181/RapidAlerts_Automated_Notification_Alert.py
+++ dags/C-181/RapidAlerts_Automated_Notification_Alert.py
@@ -1,9 +1,12 @@
+
+
 ## Aishwarya here is a basic template - We would need to build it out, but the connectivity and the table reference are correct for the 2 hour alert described in SUP-10813
 # Needs thorough testing
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 from airflow.utils.email import send_email
+from airflow.providers.sftp.hooks.sftp import SFTPHook
 import os
 import pandas as pd
 import logging
@@ -12,17 +15,49 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
+# Define the DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 3, 17),
+    'on_failure_callback': failure_callback,
+}
+
+dag = DAG(
+    'RapidAerts_Automated_Notification',
+    default_args=default_args,
+    description='This DAG retrieves the syslog file from a VM where it is collected and stores it for future audits with HITRUST implications',
+    schedule_interval='@daily',
+    catchup=False,
+    tags=['C-181'],
+)
+
+
 # Define the failure callback function
 def failure_callback(context):
     dag_name = context['dag'].dag_id
     dag_file_path = context['dag'].fileloc
     send_email(
-        to='tlamond@konza.org;ddooley@konza.org;cclark@konza.org;jdenson@konza.org',
+        to='RapidAlerts_PM_C-181@konza.org;ethompson@konza.org',
         subject=f'Task Failed in DAG: {dag_name}',
         html_content=f"Task {context['task_instance_key_str']} failed in DAG: {dag_name}. DAG source file: {dag_file_path}. Check the logs for more details."
     )
+@task(dag=dag)
 def crawler_reference_alert(**kwargs):    
-    sql_hook = MySqlHook(mysql_conn_id="prd-az1-sqlw2-airflowconnection")  # Replace with your connection ID
-    query = "SELECT * FROM _dashboard_maintenance.crawler_reference_table;"  # Replace with your SQL query
+    sql_hook = MySqlHook(mysql_conn_id="prd-az1-sqlw3-airflowconnection")  # Replace with your connection ID
+    query = "SELECT (md5(client_security_groupings_admins_name)) as ConnectionID_md5 FROM _dashboard_maintenance.crawler_reference_table where client_reference_folder IS NOT NULL;"  # Replace with your SQL query
     dfCrawlerAudit = sql_hook.get_pandas_df(query)
+    for index, row in dfCrawlerAudit.iterrows():
+        logging.info(f'row: {row}')
+        print(row)
+        ## Use ConnectionID_md5 to cycle through production connection IDs to connect to SFTPs here
+
+crawler_alert = crawler_reference_alert()
+
+
+crawler_alert
+    
+if __name__ == "__main__":
+    dag.cli()
 
