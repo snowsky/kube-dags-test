2fbc3647acf21e6532700d73830fd2ad003e62b3
biakonza <146449798+biakonza@users.noreply.github.com>
2025-03-18 17:20:52 -0500

    Update CSG_CSGA_Automated_Notification.py

--- dags/C-181/CSG_CSGA_Automated_Notification.py
+++ dags/C-181/CSG_CSGA_Automated_Notification.py
@@ -48,52 +48,67 @@ dag_file_path_base = __file__
 @task(dag=dag)
 def crawler_reference_alert(**kwargs):
     sql_hook = MySqlHook(mysql_conn_id="prd-az1-sqlw3-mysql-airflowconnection")
-    query = "SELECT (md5(client_reference_folder)) as ConnectionID_md5, client_reference_folder FROM _dashboard_maintenance.crawler_reference_table where client_reference_folder IS NOT NULL;"
+    sql_hook_old = MySqlHook(mysql_conn_id="prd-az1-sqlw2-airflowconnection")
+    query = "SELECT (md5(client_reference_folder)) as client_md5, client_reference_folder FROM _dashboard_maintenance.crawler_reference_table where client_reference_folder IS NOT NULL;"
     dfCrawlerAudit = sql_hook.get_pandas_df(query)
     
     for index, row in dfCrawlerAudit.iterrows():
-        connection_id_md5 = row['ConnectionID_md5']
+        client_md5 = row['client_md5']
         client_reference_folder = row['client_reference_folder']
-        logging.info(f'Processing connection ID: {connection_id_md5} for Client Folder Reference {client_reference_folder}')
+        logging.info(f'Processing connection ID: {client_md5} for Client Folder Reference {client_reference_folder}')
 
-        try:
-            sftp_hook = SFTPHook(ssh_conn_id=connection_id_md5)
-        except Exception as e:
-            logging.warning(f'Failed to use ssh_conn_id: {e}')
-            try:
-                sftp_hook = SFTPHook(sftp_conn_id=connection_id_md5)
-            except Exception as e:
-                logging.error(f'Failed to use sftp_conn_id: {e}')
-                continue
-
-        try:
-            with sftp_hook.get_conn() as sftp_client:
-                files = sftp_client.listdir_attr()
-                csv_files = [file for file in files if file.filename.endswith('.csv')]
-                logging.info(f'CSV files found: {csv_files}')
-                
-                for file in csv_files:
-                    modified_time = pd.to_datetime(file.st_mtime, unit='s')
-                    logging.info(f'File: {file.filename}, Modified Date: {modified_time}')
-                    
-                    # Check against database entry
-                    db_query = f"SELECT modified_date FROM clientresults.file_modification_table WHERE filename = '{file.filename}' and client_id_md5 = '{connection_id_md5}'"
-                    dfFileMod = sql_hook.get_pandas_df(db_query)
-                    
-                    if dfFileMod.empty or modified_time > dfFileMod['modified_date'].max():
-                        send_email_alert(file.filename, modified_time,client_reference_folder)
+        # Check against database entry in production W3 CSGA
+        db_query = f"select Client, event_timestamp, md5(Client) as md5 from clientresults.client_security_groupings_approved  WHERE client_id_md5 = '{client_md5}' LIMIT 1"
+        dfCurrentCSGA = sql_hook.get_pandas_df(db_query)
+        if dfCurrentCSGA.empty: #Use the old DB W2 if needed
+            db_query = f"select Client, event_timestamp, md5(Client) as md5 from clientresults.client_security_groupings_approved  WHERE client_id_md5 = '{client_md5}' LIMIT 1"
+            dfCurrentCSGA = sql_hook_old.get_pandas_df(db_query)
+        Client = dfCurrentCSGA['Client']
+        CSG_or_CSGA = 'CSGA'
+        modified_time = dfCurrentCSGA['event_timestamp']
+        md5 = dfCurrentCSGA['md5']
+        # Check against database entry
+        db_query = f"SELECT modified_date FROM clientresults.csg_modification_table WHERE client_id_md5 = '{client_md5}'"
+        dfModificationCheck = sql_hook.get_pandas_df(db_query)
+        if dfModificationCheck.empty or modified_time > dfModificationCheck['modified_date'].max():
+            send_email_alert(CSG_or_CSGA, modified_time,Client)
                         
-                        # Update the database with the new modified date
-                        update_query = f"REPLACE INTO clientresults.file_modification_table (filename, modified_date,client_id_md5) VALUES ('{file.filename}', '{modified_time}', '{connection_id_md5}')"
-                        sql_hook.run(update_query)
+            # Update the database with the new modified date
+            update_query = f"REPLACE INTO clientresults.csg_modification_table (Client,CSG_or_CSGA, modified_date,client_id_md5) VALUES ('{Client}','{CSG_or_CSGA}',  '{modified_time}', '{md5}')"
+            sql_hook.run(update_query)
         except Exception as e:
             logging.error(f'Error Occurred: {e}')
-def send_email_alert(filename, modified_time,client_id):
+
+
+        
+        # Check against database entry in production W3 CSGA
+        db_query = f"select Client, event_timestamp, md5(Client) as md5 from clientresults.client_security_groupings  WHERE client_id_md5 = '{client_md5}' LIMIT 1"
+        dfCurrentCSG = sql_hook.get_pandas_df(db_query)
+        if dfCurrentCSG.empty: #Use the old DB W2 if needed
+            db_query = f"select Client, event_timestamp, md5(Client) as md5 from clientresults.client_security_groupings  WHERE client_id_md5 = '{client_md5}' LIMIT 1"
+            dfCurrentCSG = sql_hook_old.get_pandas_df(db_query)
+        Client = dfCurrentCSG['Client']
+        CSG_or_CSGA = 'CSG'
+        modified_time = dfCurrentCSG['event_timestamp']
+        md5 = dfCurrentCSG['md5']
+        # Check against database entry
+        db_query = f"SELECT modified_date FROM clientresults.csg_modification_table WHERE client_id_md5 = '{client_md5}'"
+        dfModificationCheck = sql_hook.get_pandas_df(db_query)
+        if dfModificationCheck.empty or modified_time > dfModificationCheck['modified_date'].max():
+            send_email_alert(CSG_or_CSGA, modified_time,Client)
+                        
+            # Update the database with the new modified date
+            update_query = f"REPLACE INTO clientresults.csg_modification_table (Client,CSG_or_CSGA, modified_date,client_id_md5) VALUES ('{Client}','{CSG_or_CSGA}',  '{modified_time}', '{md5}')"
+            sql_hook.run(update_query)
+        except Exception as e:
+            logging.error(f'Error Occurred: {e}')
+       
+def send_email_alert(CSG_or_CSGA_indicator,modified_time,client_id):
     send_email(
         #to='RapidAlerts_PM_C-181@konza.org;ethompson@konza.org',
         to='ethompson@konza.org',
         subject=f'KONZA has received a new file to the SFTP for Client ID {client_id} (C-181)',
-        html_content=f"Newly Modified or New CSV File: {filename} - Client Identifier/Folder Name {client_id} - Reporting DAG: {dag_name_base}. DAG source file: {dag_file_path_base}. Check the logs for more details."
+        html_content=f"Newly Modified CSG in Category: {CSG_or_CSGA_indicator} - Client Identifier/Folder Name {client_id} - Reporting DAG: {dag_name_base}. DAG source file: {dag_file_path_base}. Check the logs for more details."
     )
 
 crawler_alert = crawler_reference_alert()
