31b3c171c2d3dbb19bf74d0719a9f2473db4f2bb
biakonza <146449798+biakonza@users.noreply.github.com>
2025-04-15 17:12:55 -0500

    Update common.py

--- dags/sla113/common.py
+++ dags/sla113/common.py
@@ -1,5 +1,5 @@
 
-CONNECTION_NAME = 'qa-az1-sqlw2-airflowconnection'
+CONNECTION_NAME = 'prd-az1-sqlw3-mysql-airflowconnection'
 SOURCE_FILES_DIRECTORY = '/source-biakonzasftp/C-9/SLA-113/'
 TMP_OUTPUT_DIR = '/source-biakonzasftp/C-9/SLA-11-output/'
 TARGET_TABLE = 'person_master._mpi'
