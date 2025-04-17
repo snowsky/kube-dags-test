8cc99384ccf5bbdd16e7f78e88d8c3766d19b4f6
biakonza <146449798+biakonza@users.noreply.github.com>
2025-04-17 10:21:57 -0500

    Update RapidAlerts_Automated_Notification_Alert.py

--- dags/C-181/RapidAlerts_Automated_Notification_Alert.py
+++ dags/C-181/RapidAlerts_Automated_Notification_Alert.py
@@ -123,8 +123,8 @@ def send_email_alert(filename, modified_time,client_id):
         ##External
         #to=f'{client_distribution_list_notifier};ethompson@konza.org',
         ##Internal Testing - RapidAlerts_PM_C-181@konza.org (as of 3/26/2025)
-        to='ethompson@konza.org',
-        #to='RapidAlerts_PM_C-181@konza.org;ethompson@konza.org',
+        #to='ethompson@konza.org',
+        to='RapidAlerts_PM_C-181@konza.org;ethompson@konza.org',
         #to='ethompson@konza.org;tlamond@konza.org;slewis@konza.org;cclark@konza.org',
         subject=f'KONZA has received a new file to the SFTP for Client ID {client_id} (C-181)',
         #html_content=f"Newly Modified or New CSV File: {filename} - Client Identifier/Folder Name:  {client_id} - Reporting DAG: {dag_name_base}. DAG source file: {dag_file_path_base}. Check the logs for more details."
