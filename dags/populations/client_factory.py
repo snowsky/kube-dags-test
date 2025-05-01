a2af7d1ebdd76cb300444862fea129368e2920cd
ddooleykonzaorg <168588350+ddooleykonzaorg@users.noreply.github.com>
2025-05-01 15:24:23 -0500

    Sla145 pf1447 pop (#195)
    * resolved rebase conflicts
    
    * setting the hourly csga runs
    
    * change local sftp path
    
    * adding the piped delimited data driven builds

--- dags/populations/client_factory.py
+++ dags/populations/client_factory.py
@@ -1,7 +1,7 @@
 from populations.client_profiles.data_driven import DataDrivenClientProfile
-#from populations.client_profiles.data_driven_piped import DataDrivenPipedClientProfile
+from populations.client_profiles.data_driven_piped import DataDrivenPipedClientProfile
 from populations.client_profiles.csv_internally_delivered import CsvInternallyDeliveredClientProfile
-#from populations.client_profiles.excel_internally_delivered import ExcelInternallyDeliveredClientProfile
+#from populations.client_profiles.hiawatha_community_hospital_internally_delivered import HiawathaCommunityHospitalInternallyDeliveredClientProfile
 from typing import List, Tuple
 import os
 
@@ -10,6 +10,8 @@ import os
 def get_client_profile(folder_name, ending_db, frequency, facility_ids, airflow_client_profile, internal_source_path="/source-biakonzasftp/C-25/"):
     if airflow_client_profile == 'data_driven':
         return DataDrivenClientProfile(folder_name, ending_db)
+    elif airflow_client_profile == 'data_driven_piped':
+        return DataDrivenPipedClientProfile(folder_name, ending_db)
     elif airflow_client_profile == 'csv_internally_delivered':
         client_source_path = os.path.join(internal_source_path, folder_name)
         if not os.path.exists(client_source_path):
@@ -21,7 +23,5 @@ def get_client_profile(folder_name, ending_db, frequency, facility_ids, airflow_
             raise ValueError(f"multiple files found in: {client_source_path}")
         input_file_path = os.path.join(client_source_path, file_names[0])
         return CsvInternallyDeliveredClientProfile(input_file_path, folder_name, ending_db)
-    #elif airflow_client_profile == 'excel_internally_delivered':
-    #    return ExcelInternallyDeliveredClientProfile(input_file_path=input_file_path, folder_name=folder_name, ending_db=ending_db)
     else:
         raise ValueError(f"Unknown client profile: {folder_name}")
---
+++ dags/populations/client_profiles/data_driven_piped.py
@@ -0,0 +1,87 @@
+from populations.client_profiles.client_profie import ClientProfile
+from populations.utils import execute_query
+from airflow.models import Variable
+import logging
+
+class DataDrivenPipedClientProfile(ClientProfile):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        #super().__init__()
+        #self._client_name = client_name
+        #self._data_source = "clientdatashare.target_population"
+        #self._target_table_prefix = "tp"
+        #self._ending_db = ending_db
+        #self._mpi_crosswalk = False
+        #self._csg = True
+
+    #logging.info(f'client_name line 17 should be {client_name}')
+    
+    
+    def process_population_data(self, facility_ids):
+        self._query_population_data()
+        self._query_and_insert_mpi(facility_ids)
+        logging.info(f'facility ids should be {facility_ids}')
+
+
+    def _query_population_data(self):
+        query = f"""
+                DROP TABLE IF EXISTS {self._schema}.{self.target_table};
+                CREATE TABLE {self._schema}.{self.target_table} AS
+                SELECT MPI, provider_first_name
+                FROM {self._data_source} WHERE 1 = 0;
+                """
+        execute_query(query, self._conn_id)
+
+    def _query_and_insert_mpi(self, facility_ids):
+        mpi_source_table = 'pa_thirtysix_month_lookback'
+        #facility_ids_unpiped = Variable.get(facility_ids.replace('|', ','), deserialize_json=True) 
+        facility_ids_unpiped = facility_ids.replace('|', ',')
+        quoted_values = ', '.join([f"'{value}'" for value in facility_ids_unpiped.split(',')])
+        logging.info(f'facility ids unpiped should be {facility_ids_unpiped}')
+        logging.info(f'facility ids unpiped quote should be {quoted_values}')
+        query = f"""
+                INSERT INTO {self._schema}.{self.target_table} (MPI)
+                (SELECT MPI FROM temp.{mpi_source_table}
+                WHERE unit_id IN ({quoted_values})
+                AND admitted > DATE_SUB(now(), INTERVAL 18 MONTH)
+                AND MPI <> '999999999999999'
+                group by MPI);
+                """
+        execute_query(query, self._conn_id)
+    
+    @property
+    def client_name(self):
+        return self._client_name
+
+    #@property
+    #@abstractmethod
+    #def target_table(self):
+    #    pass
+
+    @property
+    def target_table(self):
+        return f"{self._target_table_prefix}{self.client_name_fmt}"
+    
+    @property
+    def ending_db(self):
+        return self._ending_db
+
+    @property
+    def mpi_crosswalk(self):
+        return self._mpi_crosswalk
+
+    @property
+    def csg(self):
+        return self._csg
+
+    @property
+    def conn_id(self):
+        return self._conn_id
+
+    @property
+    def schema(self):
+        return self._schema
+
+    @property
+    def client_name_fmt(self):
+        return self.client_name.replace(' ', '').lower()
