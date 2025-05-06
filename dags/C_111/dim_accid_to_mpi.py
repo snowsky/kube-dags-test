545b0a34e7f7445d4603686eaea9be3ae715b9bb
bogdanstate <bogdan@scie.nz>
2025-05-07 06:12:11 +1200

    Update dim_accid_to_mpi.py (#198)

--- dags/C_111/dim_accid_to_mpi.py
+++ dags/C_111/dim_accid_to_mpi.py
@@ -14,13 +14,14 @@ from airflow.hooks.base import BaseHook
 from airflow.exceptions import AirflowException
 from lib.operators.konza_trino_operator import KonzaTrinoOperator
 from datetime import timedelta
-from airflow import DAG, task
+from airflow import DAG
+from airflow.decorators import task
 from airflow.operators.python import ShortCircuitOperator, PythonOperator
 from datetime import datetime
 from kubernetes import client, config
 
 @task
-def scale_trino_workers(namespace="trino", deployment_name="trino-worker", replicas=3, downscaling_okay=True):
+def scale_trino_workers(namespace="trino", deployment_name="trino-worker", replicas=3, downscaling_okay=True, delay=120):
     """
     Scale the Kubernetes deployment named 'trino-worker' to a specified number of replicas.
     
@@ -55,6 +56,7 @@ def scale_trino_workers(namespace="trino", deployment_name="trino-worker", repli
             )
             
             print(f"Successfully scaled {deployment_name} to {replicas} replicas in namespace {namespace}")
+            time.sleep(delay)
             return True
         else:
             print(f"Skipping downscaling from {deployment.spec.replicas} to {replicas} since downscaling_okay set to False.")
