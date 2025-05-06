5747be1eca41b4819180a00d03cdba72a7992315
bogdanstate <bogdan@scie.nz>
2025-05-07 06:07:21 +1200

    Added upscale / downscale instructions to dim_accid_to_mpi DAG (#197)
    This PR adds two tasks for upscaling and downscaling the number of desired Trino workers in preparation for running a large query.
    
    Note that the code hasn't been tested yet, on account of the test environment being broken -- we should do this during our meeting tomorrow.

--- dags/C_111/dim_accid_to_mpi.py
+++ dags/C_111/dim_accid_to_mpi.py
@@ -14,9 +14,54 @@ from airflow.hooks.base import BaseHook
 from airflow.exceptions import AirflowException
 from lib.operators.konza_trino_operator import KonzaTrinoOperator
 from datetime import timedelta
-from airflow import DAG
+from airflow import DAG, task
 from airflow.operators.python import ShortCircuitOperator, PythonOperator
 from datetime import datetime
+from kubernetes import client, config
+
+@task
+def scale_trino_workers(namespace="trino", deployment_name="trino-worker", replicas=3, downscaling_okay=True):
+    """
+    Scale the Kubernetes deployment named 'trino-worker' to a specified number of replicas.
+    
+    Args:
+        namespace (str): Kubernetes namespace where the deployment exists
+        deployment_name (str): Name of the deployment to scale
+        replicas (int): Desired number of replicas (workers)
+        downscaling_okay (bool): If this is set to True, the function will reduce the number of workers 
+          if the current number of replicas in the cluster is greater than the desired number of workers.
+    """
+    try:
+        # Load Kubernetes configuration
+        config.load_incluster_config()  # For running inside a pod in Kubernetes cluster
+        
+        # Create API client
+        apps_v1_api = client.AppsV1Api()
+        
+        # Get current deployment
+        deployment = apps_v1_api.read_namespaced_deployment(
+            name=deployment_name,
+            namespace=namespace
+        )
+
+        if downscaling_okay or deployment.spec.replicas < replicas:
+            deployment.spec.replicas = replicas
+            
+            # Apply the update
+            apps_v1_api.patch_namespaced_deployment(
+                name=deployment_name,
+                namespace=namespace,
+                body=deployment
+            )
+            
+            print(f"Successfully scaled {deployment_name} to {replicas} replicas in namespace {namespace}")
+            return True
+        else:
+            print(f"Skipping downscaling from {deployment.spec.replicas} to {replicas} since downscaling_okay set to False.")
+    except Exception as e:
+        print(f"Error scaling deployment: {e}")
+        raise
+
 
 default_args = {
     'owner': 'airflow',
@@ -127,6 +172,9 @@ with DAG(
         """,
     )
 
+    upscale = scale_trino_workers(20, downscaling_okay=False)
+    
+    upscale >> create_tmp_dim_accid_to_mpi
     create_dim_accid_to_mpi >> populate_dim_accid_to_mpi
     drop_tmp_dim_accid_to_mpi >> create_tmp_dim_accid_to_mpi
     create_tmp_dim_accid_to_mpi >> create_dim_accid_to_mpi_grouped
@@ -134,3 +182,7 @@ with DAG(
     create_dim_accid_to_mpi_grouped >> populate_dim_accid_to_mpi
     populate_dim_accid_to_mpi >> cleanup_tmp_dim_accid_to_mpi_grouped
     populate_dim_accid_to_mpi >> cleanup_tmp_dim_accid_to_mpi 
+    # optional: note that this may interfere with other DAGs
+    
+    downscale = scale_trino_workers(1, downscaling_okay=True)
+    populate_dim_accid_to_mpi >> downscale
