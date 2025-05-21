b411965b4a1c1b24880ed46340d8cf64e9aa1561
biakonza <146449798+biakonza@users.noreply.github.com>
2025-05-21 15:52:56 -0500

    Update bucket_master_patient_index.py

--- dags/C_35/bucket_master_patient_index.py
+++ dags/C_35/bucket_master_patient_index.py
@@ -1,9 +1,59 @@
-from airflow.hooks.base import BaseHook
 import trino
 import mysql.connector
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import ShortCircuitOperator, PythonOperator
+from airflow.decorators import task
 from datetime import datetime
+from airflow.hooks.base import BaseHook
+from airflow.exceptions import AirflowException
+from lib.operators.konza_trino_operator import KonzaTrinoOperator
+from kubernetes import client, config
+
+def scale_trino_workers(namespace="trino", deployment_name="trino-worker", replicas=3, downscaling_okay=True, delay=120):
+    """
+    Scale the Kubernetes deployment named 'trino-worker' to a specified number of replicas.
+    
+    Args:
+        namespace (str): Kubernetes namespace where the deployment exists
+        deployment_name (str): Name of the deployment to scale
+        replicas (int): Desired number of replicas (workers)
+        downscaling_okay (bool): If this is set to True, the function will reduce the number of workers 
+          if the current number of replicas in the cluster is greater than the desired number of workers.
+    """
+    try:
+        # Load Kubernetes configuration
+        config.load_incluster_config()  # For running inside a pod in Kubernetes cluster
+        
+        # Create API client
+        apps_v1_api = client.AppsV1Api()
+        
+        # Get current deployment
+        deployment = apps_v1_api.read_namespaced_deployment(
+            name=deployment_name,
+            namespace=namespace
+        )
+
+        if downscaling_okay or deployment.spec.replicas < replicas:
+            deployment.spec.replicas = replicas
+            
+            # Apply the update
+            apps_v1_api.patch_namespaced_deployment(
+                name=deployment_name,
+                namespace=namespace,
+                body=deployment
+            )
+            
+            print(f"Successfully scaled {deployment_name} to {replicas} replicas in namespace {namespace}")
+            time.sleep(delay)
+            return True
+        else:
+            print(f"Skipping downscaling from {deployment.spec.replicas} to {replicas} since downscaling_okay set to False.")
+    except Exception as e:
+        print(f"Error scaling deployment: {e}")
+        raise
+
+
+
 def execute_trino_queries(**kwargs):
     ds = kwargs['ds']
     # Retrieve the connection details
@@ -73,4 +123,7 @@ with DAG(
         python_callable=execute_trino_queries,
         provide_context=True,
     )
-    execute_queries_task
+    upscale = scale_trino_workers(replicas=20, downscaling_okay=False)
+    downscale = scale_trino_workers(replicas=1, downscaling_okay=True)
+    upscale >> execute_queries_task >> downscale
+    
