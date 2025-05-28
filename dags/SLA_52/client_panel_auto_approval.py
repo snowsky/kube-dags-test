fd4ab52c45145ad8c5fc5658311a0cf70301e132
biakonza <146449798+biakonza@users.noreply.github.com>
2025-05-28 14:56:07 -0500

    Update client_panel_auto_approval.py

--- dags/SLA_52/client_panel_auto_approval.py
+++ dags/SLA_52/client_panel_auto_approval.py
@@ -1,3 +1,4 @@
+from airflow.decorators import dag, task
 from airflow import DAG
 import pandas as pd
 from airflow.operators.python import PythonOperator
@@ -7,7 +8,6 @@ from airflow.providers.mysql.operators.mysql import MySqlOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from datetime import datetime,timedelta
-from airflow.decorators import task
 from airflow.hooks.base_hook import BaseHook
 import logging
 import paramiko
