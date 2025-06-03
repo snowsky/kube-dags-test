fb36030ea1c6d9bebcf3d27709ec30b51171e6c1
biakonza <146449798+biakonza@users.noreply.github.com>
2025-06-03 09:51:22 -0500

    Update C_126_L_69_mapped.py

--- dags/C-126/C_126_L_69_mapped.py
+++ dags/C-126/C_126_L_69_mapped.py
@@ -55,6 +55,8 @@ with DAG(
     dag_id='C_126_L_69_mapped',
     default_args=default_args,
     schedule='@hourly',
+    start_date=days_ago(1),
+    catchup=False,
     tags=['example', 'C-126', 'L-69'],
     concurrency=PARALLEL_TASK_LIMIT,
     params={
