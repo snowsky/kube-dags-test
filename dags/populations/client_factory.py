fbb5fa2773e3860c55328dbf390baf266d5b11a3
ddooleykonzaorg <168588350+ddooleykonzaorg@users.noreply.github.com>
2025-06-03 10:01:33 -0500

    Sla145 pf1447 pop (#228)
    * resolved rebase conflicts
    
    * setting the hourly csga runs
    
    * change local sftp path
    
    * adding the piped delimited data driven builds
    
    * pushing the internal csv template example client for SUP-14597 and SUP-12807
    
    ---------
    
    Co-authored-by: biakonza <146449798+biakonza@users.noreply.github.com>

--- dags/populations/client_factory.py
+++ dags/populations/client_factory.py
@@ -1,18 +1,19 @@
 from populations.client_profiles.data_driven import DataDrivenClientProfile
 from populations.client_profiles.data_driven_piped import DataDrivenPipedClientProfile
 from populations.client_profiles.csv_internally_delivered import CsvInternallyDeliveredClientProfile
+from populations.client_profiles.community_mental_health_center_of_crawford_county_internally_delivered import CommunityMentalHealthCenterOfCrawfordCountyInternallyDeliveredClientProfile
+
 #from populations.client_profiles.hiawatha_community_hospital_internally_delivered import HiawathaCommunityHospitalInternallyDeliveredClientProfile
 from typing import List, Tuple
 import os
 
 # Change internal_source_path to /data/biakonzasftp/ instead of /source-biakonzasftp/ when testing locally
 
-def get_client_profile(folder_name, ending_db, frequency, facility_ids, airflow_client_profile, internal_source_path="/source-biakonzasftp/C-25/"):
-    if airflow_client_profile == 'data_driven':
-        return DataDrivenClientProfile(folder_name, ending_db)
-    elif airflow_client_profile == 'data_driven_piped':
-        return DataDrivenPipedClientProfile(folder_name, ending_db)
-    elif airflow_client_profile == 'csv_internally_delivered':
+def get_client_profile(folder_name, ending_db, frequency, facility_ids, airflow_client_profile, internal_source_path="/source-biakonzasftp/C-25/Client_Lists/"):
+
+
+
+    elif airflow_client_profile == 'community_mental_health_center_of_crawford_county_internally_delivered':
         client_source_path = os.path.join(internal_source_path, folder_name)
         if not os.path.exists(client_source_path):
             raise ValueError(f"path does not exist: {client_source_path}")
@@ -22,6 +23,6 @@ def get_client_profile(folder_name, ending_db, frequency, facility_ids, airflow_
         elif len(file_names) > 1:
             raise ValueError(f"multiple files found in: {client_source_path}")
         input_file_path = os.path.join(client_source_path, file_names[0])
-        return CsvInternallyDeliveredClientProfile(input_file_path, folder_name, ending_db)
+        return CommunityMentalHealthCenterOfCrawfordCountyInternallyDeliveredClientProfile(input_file_path, folder_name, ending_db)
     else:
         raise ValueError(f"Unknown client profile: {folder_name}")
---
+++ dags/populations/create_population_definitions_migration_testing.py
@@ -0,0 +1,66 @@
+from airflow import DAG
+from airflow.operators.python import get_current_context
+from airflow.providers.mysql.operators.mysql import MySqlOperator
+from airflow.providers.mysql.hooks.mysql import MySqlHook
+from airflow.utils.dates import days_ago
+from airflow.decorators import task
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from datetime import datetime
+import logging
+#from lib.operators.mysql.konza_returning_mysql_operator import KonzaReturningMySqlOperator
+
+from populations.common import CONNECTION_NAME
+
+class ReturningMySqlOperator(MySqlOperator):
+    def execute(self, context):
+        self.log.info('Executing: %s', self.sql)
+        hook = MySqlHook(mysql_conn_id=self.conn_id)
+        return hook.get_records(
+            self.sql,
+            parameters=self.parameters
+        )
+
+
+default_args = {
+    'owner': 'airflow',
+}
+with DAG(
+    'create_population_definitions_migration_testing',
+    default_args=default_args,
+    schedule=None,
+    #schedule_interval='@monthly',
+    #start_date=datetime(2025, 2, 1),
+    tags=['example', 'population-definitions'],
+) as dag:
+
+    @task(map_index_template="{{ client_name }}")
+    def process_client(row, **kwargs):
+        client_name = row[0]
+        context = get_current_context()
+        context["client_name"] = client_name
+        trigger = TriggerDagRunOperator(
+            task_id=f"target_pop_{client_name.replace(' ', '_')}",
+            trigger_dag_id='target_population',
+            ##conf={'client_name': client_name, 'frequency': row[1], 'facility_ids': row[2]}, 
+            conf={'client_name': row[0], 'folder_name': row[0], 'ending_db': row[1], 'frequency': row[2], 'facility_ids': row[3], 'airflow_client_profile': row[4]},
+
+            dag=dag,
+        )
+        trigger.execute(context=kwargs)
+
+
+    clients_to_process = ReturningMySqlOperator(
+        task_id='clients_to_process',
+        mysql_conn_id=CONNECTION_NAME,
+        sql=f"""
+        SELECT folder_name, ending_db, frequency, facility_ids, airflow_client_profile
+        FROM _dashboard_requests.clients_to_process
+        WHERE folder_name is not null and active = 1 and airflow = 1 
+        AND (
+            frequency = 'TESTING'
+        )
+        """,
+        dag=dag
+    )
+
+    result = process_client.expand(row=clients_to_process.output)
