11c1ba08c9ec5ecb6571549480100d19f41d1eca
biakonza <146449798+biakonza@users.noreply.github.com>
2025-07-24 14:32:45 -0500

    Update CardAdditionDag.py

--- dags/C_165/CardAdditionDag.py
+++ dags/C_165/CardAdditionDag.py
@@ -1,4 +1,5 @@
 from airflow import DAG
+import logging
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
