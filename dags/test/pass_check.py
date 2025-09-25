a30d89b11900b88f880ea64b44d1803f291e7275
biakonza <146449798+biakonza@users.noreply.github.com>
2025-09-25 09:08:20 -0500

    Update pass_check.py

--- dags/test/pass_check.py
+++ dags/test/pass_check.py
@@ -1,17 +1,12 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.decorators import task
 from airflow.hooks.base import BaseHook
 from datetime import datetime
-
-def get_conn_password(conn_id: str, **kwargs):
-    conn = BaseHook.get_connection(conn_id)
-    password = conn.password
-    return password  # This will be pushed to XCom automatically
 
 with DAG(
     dag_id='retrieve_conn_password',
     start_date=datetime(2023, 1, 1),
-    schedule=None,  # Airflow 3.x uses 'schedule' instead of 'schedule_interval'
+    schedule=None,
     catchup=False,
     tags=['example'],
     params={
@@ -19,8 +14,9 @@ with DAG(
     }
 ) as dag:
 
-    retrieve_password = PythonOperator(
-        task_id='get_password',
-        python_callable=get_conn_password,
-        op_args=["{{ params.conn_id }}"],
-    )
+    @task
+    def get_conn_password(conn_id: str):
+        conn = BaseHook.get_connection(conn_id)
+        return conn.password
+
+    get_conn_password(conn_id="{{ params.conn_id }}")
